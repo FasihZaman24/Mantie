196c8c580b1285a84516357edef48f4c8a7ddaa2
Fasih Zaman <148210474+FasihZaman24@users.noreply.github.com>
2023-12-07 09:52:00 +0100

    Tab View

---
+++ Mantie/Mantie/Views/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  Mantie
+//
+//  Created by Muhammad Fasih uz Zaman Siddiqui on 07/12/23.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuView()
+}
