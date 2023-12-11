c2a3b06f8949847a4bffad86dc32dff0581fe79c
Fasih Zaman <148210474+FasihZaman24@users.noreply.github.com>
2023-12-11 11:09:01 +0100

    Update

---
+++ Mantie/LotteAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  LotteAnimationView.swift
+//  Mantie
+//
+//  Created by Muhammad Fasih uz Zaman Siddiqui on 10/12/23.
+//
+
+import SwiftUI
+
+struct LotteAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LotteAnimationView()
+}
