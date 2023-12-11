f2f0d2442c39f3ab7487c4fa687660a328bd86cc
Fasih Zaman <148210474+FasihZaman24@users.noreply.github.com>
2023-12-11 11:09:36 +0100

    Update

--- Mantie/LotteAnimationView.swift
+++ Mantie/LotteAnimationView.swift
@@ -5,14 +5,37 @@
 //  Created by Muhammad Fasih uz Zaman Siddiqui on 10/12/23.
 //
 
-import SwiftUI
-
-struct LotteAnimationView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    LotteAnimationView()
-}
+//import SwiftUI
+//import Lottie
+//
+//struct LottieAnimationView: UIViewRepresentable {
+//    var filename: String
+//
+//    func makeUIView(context: Self.Context) -> UIView {
+//        let view = UIView(frame: .zero)
+//        let animationView = AnimationView()
+//        animationView.animation = Animation.named(filename)
+//        animationView.contentMode = .scaleAspectFit
+//        animationView.loopMode = .loop
+//        animationView.play()
+//
+//        animationView.translatesAutoresizingMaskIntoConstraints = false
+//        view.addSubview(animationView)
+//
+//        NSLayoutConstraint.activate([
+//            animationView.widthAnchor.constraint(equalTo: view.widthAnchor),
+//            animationView.heightAnchor.constraint(equalTo: view.heightAnchor)
+//        ])
+//
+//        return view
+//    }
+//
+//    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<LottieAnimationView>) {
+//        // UIView update code
+//    }
+//}
+//
+//
+//#Preview {
+//    LotteAnimationView()
+//}
