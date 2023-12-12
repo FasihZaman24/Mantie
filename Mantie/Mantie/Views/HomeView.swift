8bb10f3c60bee2faaaee3b125ab312784d1d6928
Fasih Zaman <148210474+FasihZaman24@users.noreply.github.com>
2023-12-12 12:19:54 +0100

    Tuesday

--- Mantie/Mantie/Views/HomeView.swift
+++ Mantie/Mantie/Views/HomeView.swift
@@ -12,30 +12,6 @@ import SwiftUI
 import MapKit
 
 
-// CLASS FOR LOCATION SEARCH DROPDOWN
-//class LocationSearchCompleter: NSObject, ObservableObject, MKLocalSearchCompleterDelegate {
-//    @Published var suggestions: [MKLocalSearchCompletion] = []
-//    var completer: MKLocalSearchCompleter
-//
-//    override init() {
-//        completer = MKLocalSearchCompleter()
-//        super.init()
-//        completer.delegate = self
-//    }
-//
-//    func search(query: String) {
-//        completer.queryFragment = query
-//    }
-//
-//    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
-//        suggestions = completer.results
-//    }
-//
-//    func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
-//        // Handle error
-//        print("Completer failed with error: \(error.localizedDescription)")
-//    }
-//}
 
 struct HomeView: View {
     @State private var searchText = ""
@@ -96,25 +72,6 @@ struct SearchBar: View {
     }
 }
 
-
-//struct SuggestionsListView: View {
-//    var suggestions: [MKLocalSearchCompletion]
-//    var searchCompleter: LocationSearchCompleter
-//    @Binding var region: MKCoordinateRegion
-//
-//    var body: some View {
-//        List {
-//            ForEach(suggestions, id: \.self) { suggestion in
-//                Button(action: {
-//                    selectSuggestion(suggestion)
-//                }) {
-//                    Text(suggestion.title)
-//                }
-//            }
-//        }
-//        .listStyle(PlainListStyle())
-//    }
-
 #Preview {
     HomeView()
 }
@@ -152,83 +109,4 @@ struct SearchBar: View {
 
 
 
-
-//import SwiftUI
-//import MapKit
-//
-//struct HomeView: View {
-//    @State private var searchText = ""
-//        @State private var region = MKCoordinateRegion(
-//            center: CLLocationCoordinate2D(latitude: 34.0522, longitude: -118.2437), // Default to Los Angeles, for example
-//            span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-//        )
-//
-//    var body: some View {
-//        VStack {
-//            
-//            HStack {
-//                TextField("Enter location", text: $searchText)
-//                    .padding(7)
-//                    .padding(.horizontal, 25)
-//                    .background(Color(.systemGray6))
-//                    .cornerRadius(8)
-//                    .overlay(
-//                        HStack {
-//                            Image(systemName: "magnifyingglass")
-//                                .foregroundColor(.gray)
-//                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-//                                .padding(.leading, 8)
-//                            
-//                            if !searchText.isEmpty {
-//                                Button(action: {
-//                                    self.searchText = ""
-//                                }) {
-//                                    Image(systemName: "multiply.circle.fill")
-//                                        .foregroundColor(.gray)
-//                                        .padding(.trailing, 8)
-//                                }
-//                            }
-//                        }
-//                    )
-//                    .padding(.horizontal, 10)
-//                    .onSubmit {
-//                        // Initiate the search with MapKit
-//                        //searchInMapKit()
-//                    }
-//                // Style the TextField to match your design requirements
-//            }
-//            .padding()
-//            
-//            Spacer()
-//            // Add your additional UI components here
-//        }
-//        .navigationTitle("Book Luggage Storage")
-//        .navigationBarHidden(true)
-//    }
-//    
-//    
-//    
-//    
-//    
-//    
-//
-////    func searchInMapKit() {
-////        // Use MapKit's MKLocalSearch to perform the location search
-////        let searchRequest = MKLocalSearch.Request()
-////        searchRequest.naturalLanguageQuery = searchText
-////        
-////        let search = MKLocalSearch(request: searchRequest)
-////        search.start { (response, error) in
-////            guard let response = response else {
-////                print("Error: \(error?.localizedDescription ?? "Unknown error").")
-////                return
-////            }
-////
-////            // Process the response - for example, update your map view
-////            // with the search results, or store them for later use.
-////            print(response.mapItems)
-////            // You might want to move the map to the first result, etc.
-////        }
-////    }
-//}
 
--- Mantie/Mantie/Views/OnBoardingView.swift
+++ Mantie/Mantie/Views/OnBoardingView.swift
@@ -25,167 +25,170 @@ struct OnboardingView: View {
     var body: some View {
         
         
-        ZStack {
-            Color(.orangeM)
-                .ignoresSafeArea(.all, edges: .all)
-            
-            VStack(spacing: 20) {
-                // MARK: - Header
-                Spacer()
+        NavigationView {
+            ZStack {
+                Color(.orangeM)
+                    .ignoresSafeArea(.all, edges: .all)
                 
-                VStack(spacing: 0) {
-                    Text(titleText)
-                        .font(.system(size: 60))
-                        .fontWeight(.heavy)
-                        .foregroundColor(.white)
-                        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-                        .transition(.opacity)
-                        .id(titleText)
+                VStack(spacing: 20) {
+                    // MARK: - Header
+                    Spacer()
                     
-                    Text("""
-                            Trust your luggage with Mantié
-                            We help you to travel light!
-                            
-                            """)
-                        .font(.title3)
-                        .fontWeight(.light)
-                        .foregroundColor(.white)
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal, 10)
-                }
-                .opacity(isAnimating ? 1 : 0)
-                .offset(y: isAnimating ? 0 : -40)
-                .animation(.easeOut(duration: 1), value: isAnimating)
-                
-                // MARK: - Center
-                ZStack{
-                    CircleGroupView(ShapeColor: Color(.white),ShapeOpacity: 0.4)
-                        .offset(x: imageOffset.width * -1) // To make the circle go in the opposite direction as of Image.
-                        .blur(radius: abs(imageOffset.width / 7))
-                        .animation(.easeOut(duration: 1), value: imageOffset)
+                    VStack(spacing: 0) {
+                        Text(titleText)
+                            .font(.system(size: 60))
+                            .fontWeight(.heavy)
+                            .foregroundColor(.white)
+                            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                            .transition(.opacity)
+                            .id(titleText)
+                        
+                        Text("""
+                                Trust your luggage with Mantié
+                                We help you to travel light!
+                                
+                                """)
+                            .font(.title3)
+                            .fontWeight(.light)
+                            .foregroundColor(.white)
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal, 10)
+                    }
+                    .opacity(isAnimating ? 1 : 0)
+                    .offset(y: isAnimating ? 0 : -40)
+                    .animation(.easeOut(duration: 1), value: isAnimating)
                     
-                    Image(currentImageName)
-                        .resizable()
-                        .scaledToFit()
-                        .opacity(isAnimating ? 1 : 0)
-                        .animation(.easeOut(duration: 0.5), value: isAnimating)
-                        .offset(x: imageOffset.width * 1.2, y: 0)
-                        .rotationEffect(.degrees(Double(imageOffset.width / 20)))
-                        .gesture(
-                            DragGesture()
-                                .onChanged{ gesture in
-                                    if abs(imageOffset.width) <= 150 {
-                                        imageOffset = gesture.translation
+                    // MARK: - Center
+                    ZStack{
+                        CircleGroupView(ShapeColor: Color(.white),ShapeOpacity: 0.4)
+                            .offset(x: imageOffset.width * -1) // To make the circle go in the opposite direction as of Image.
+                            .blur(radius: abs(imageOffset.width / 7))
+                            .animation(.easeOut(duration: 1), value: imageOffset)
+                        
+                        Image(currentImageName)
+                            .resizable()
+                            .scaledToFit()
+                            .opacity(isAnimating ? 1 : 0)
+                            .animation(.easeOut(duration: 0.5), value: isAnimating)
+                            .offset(x: imageOffset.width * 1.2, y: 0)
+                            .rotationEffect(.degrees(Double(imageOffset.width / 20)))
+                            .gesture(
+                                DragGesture()
+                                    .onChanged{ gesture in
+                                        if abs(imageOffset.width) <= 150 {
+                                            imageOffset = gesture.translation
+                                            
+                                            withAnimation(.linear(duration: 0.25)){
+                                                indicatorOpacity = 0
+                                                titleText = "Travel"
+                                                currentImageName = "guy"
+                                            }
+                                            
+                                        }
+                                    }
+                                    .onEnded{gesture in
+                                        //withAnimation{
+                                        imageOffset = .zero
                                         
                                         withAnimation(.linear(duration: 0.25)){
-                                            indicatorOpacity = 0
-                                            titleText = "Travel"
-                                            currentImageName = "guy"
+                                            indicatorOpacity = 1
+                                            titleText = "Light."
+                                            currentImageName = "guy2"
                                         }
+                                        //}
                                         
                                     }
-                                }
-                                .onEnded{gesture in
-                                    //withAnimation{
-                                    imageOffset = .zero
-                                    
-                                    withAnimation(.linear(duration: 0.25)){
-                                        indicatorOpacity = 1
-                                        titleText = "Light."
-                                        currentImageName = "guy2"
-                                    }
-                                    //}
-                                    
-                                }
-                        
-                        )
-                        .animation(.easeOut(duration: 1), value: imageOffset)
-                        
-                }
-                Spacer()
-                .overlay(
-                    Image(systemName: "arrow.left.and.right.circle")
-                        .font(.system(size: 44, weight: .ultraLight))
-                        .foregroundColor(.white)
-                        .offset(y: 20)
-                        .opacity(isAnimating ? 1 : 0)
-                        .animation(.easeOut(duration: 1).delay(2), value: isAnimating)
-                        .opacity(indicatorOpacity)
-                    , alignment: .bottom
-                )
-                
-                Spacer()
-                
-                //MARK: - Footer
-                ZStack{
-                    NavigationLink(destination: HomeView(), isActive: $isNavigating) {
-                        EmptyView()}
-
-                    VStack (spacing: 6){
-//                        TextField("", text: $searchText, onCommit: { isNavigating = true })
-//                            .foregroundColor(.black) // Text Color
-//                            .padding(.leading, 40) // Padding to prevent icon from overlapping text
-//                            .padding() // General Padding for the TextField
-//                            .background(Color.white) // Background Color
-//                            .cornerRadius(10) // Corner Radius
-//                            .shadow(radius: 4) // Shadow
-//                            .overlay(
-//                                HStack {
-//                                    Image(systemName: "magnifyingglass")
-//                                        .foregroundColor(.gray)
-//                                        .padding(.leading)
-//                                    
-//                                    // Placeholder Text
-//                                    if searchText.isEmpty {
-//                                        Text("Enter Location")
-//                                            .foregroundColor(.gray)
-//                                            .padding(.leading, 5)
-//                                    }
-//                                    
-//                                    Spacer()
-//                                }
-//                            )
-                        
-                        
-                        
-                        Button(action: {
-                            isNavigating = true
                             
-                        }) {
-                            Text("Check Nearby Stores")
-                                .fontWeight(.bold)
-                                .foregroundColor(.white)
-                                .padding()
-                                .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.red.opacity(0.8)]), startPoint: .top, endPoint: .bottom))
-                                .cornerRadius(10)
-                                .shadow(color: .red.opacity(0.5), radius: 10, x: 0, y: 10)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color.red, lineWidth: 2)
-                                        .shadow(color: .white, radius: 10, x: -5, y: -5)
-                                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                                        .shadow(color: .black, radius: 5, x: 5, y: 5)
-                                )
-                        }
-                        Spacer()
+                            )
+                            .animation(.easeOut(duration: 1), value: imageOffset)
+                            
                     }
-                   
+                    Spacer()
+                    .overlay(
+                        Image(systemName: "arrow.left.and.right.circle")
+                            .font(.system(size: 44, weight: .ultraLight))
+                            .foregroundColor(.white)
+                            .offset(y: 20)
+                            .opacity(isAnimating ? 1 : 0)
+                            .animation(.easeOut(duration: 1).delay(2), value: isAnimating)
+                            .opacity(indicatorOpacity)
+                        , alignment: .bottom
+                    )
                     
+                    Spacer()
+                    
+                    //MARK: - Footer
+                    ZStack{
+                        NavigationLink(destination: ContentView(), isActive: $isNavigating) {
+                            EmptyView()}
 
-                }
-                //:ZStack
-                .frame(width: buttonWidth, height: 80, alignment: .center)
-                
-                
-                Spacer()
-                
-                
-            } //:VStack
-        } //:ZStack
-        .onAppear(perform: {
-            isAnimating = true
-        })
-        .preferredColorScheme(.dark)
+                        VStack (spacing: 6){
+    //                        TextField("", text: $searchText, onCommit: { isNavigating = true })
+    //                            .foregroundColor(.black) // Text Color
+    //                            .padding(.leading, 40) // Padding to prevent icon from overlapping text
+    //                            .padding() // General Padding for the TextField
+    //                            .background(Color.white) // Background Color
+    //                            .cornerRadius(10) // Corner Radius
+    //                            .shadow(radius: 4) // Shadow
+    //                            .overlay(
+    //                                HStack {
+    //                                    Image(systemName: "magnifyingglass")
+    //                                        .foregroundColor(.gray)
+    //                                        .padding(.leading)
+    //                                    
+    //                                    // Placeholder Text
+    //                                    if searchText.isEmpty {
+    //                                        Text("Enter Location")
+    //                                            .foregroundColor(.gray)
+    //                                            .padding(.leading, 5)
+    //                                    }
+    //                                    
+    //                                    Spacer()
+    //                                }
+    //                            )
+                            
+                            
+                            
+                            Button(action: {
+                                print("Button tapped. Navigating to HomeView.")
+                                isNavigating = true
+                                
+                            }) {
+                                Text("Check Nearby Stores")
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.red.opacity(0.8)]), startPoint: .top, endPoint: .bottom))
+                                    .cornerRadius(10)
+                                    .shadow(color: .red.opacity(0.5), radius: 10, x: 0, y: 10)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(Color.red, lineWidth: 2)
+                                            .shadow(color: .white, radius: 10, x: -5, y: -5)
+                                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                                            .shadow(color: .black, radius: 5, x: 5, y: 5)
+                                    )
+                            }
+                            Spacer()
+                        }
+                       
+                        
+
+                    }
+                    //:ZStack
+                    .frame(width: buttonWidth, height: 80, alignment: .center)
+                    
+                    
+                    Spacer()
+                    
+                    
+                } //:VStack
+            } //:ZStack
+            .onAppear(perform: {
+                isAnimating = true
+            })
+        .preferredColorScheme(nil)
+        }
     }
 }
 
